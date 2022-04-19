7fcd0f68969d5c167b4aca8d8815f772673854d9
nasrinnadaf <102619662+nasrinnadaf@users.noreply.github.com>
2022-04-19 19:00:06 +0430

    Add files via upload

---
+++ tamrin3.py
@@ -0,0 +1,12 @@
+lenght_number = int(input("enter lenght of list :"))
+list1 = []
+sort_list = []
+for i in range (0 , lenght_number):
+    x = int(input("enter your numb: "))
+    list1.append(x)
+    sort_list.append(x)
+    sort_list.sort()
+if list1 == sort_list :
+    print("your list is sorted. ")
+else :
+    print(" your list is not sorted! ")
